cb21bb9a29e6b80dc60a7c2b076d5f9f9d3e1352
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-10 23:07:54 +1000

    Fixed flex

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,17 +4,17 @@ import img from "../../img/alyssa.JPG";
 export default function About() {
   return (
     <div className="container mt-5 animate__animated animate__fadeIn">
-      <div className="row justify-content-around py-5">
-        <div className="col col-sm-12 col-md-6 col-lg-5">
+      <div className="row justify-content-around">
+        <div className="col-12 col-md-6 p-5">
           <div className="text-center">
             <img
               src={img}
-              className="rounded img-fluid img-thumbnail w-auto"
+              className="rounded-circle img-fluid img-thumbnail w-auto"
               alt="portfolio owner"
             ></img>
           </div>
         </div>
-        <div className="col col-sm-12 col-md-6 col-lg-5 align-self-center">
+        <div className="col-12 col-md-6 align-self-center p-3">
           <p className="fs-3">
             An accomplished and fluent communicator with strong investigation,
             problem-solving and decision-making skills enhanced by previous
