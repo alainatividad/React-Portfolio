3047ed5b2baf720150cbd2d0e3c646683941a757
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-13 21:35:00 +1000

    Added text header

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Contact() {
   return (
     <div className="container col-md-6 mt-5 pt-3 animate__animated animate__fadeIn">
-      <h1 className="mb-3 text-center">Contact Me!</h1>
+      <h2 className="mb-3 text-center">Contact Me!</h2>
       <div className="mb-3">
         <label htmlFor="validationDefault01" className="form-label">
           Name
@@ -48,7 +48,6 @@ export default function Contact() {
         <a
           href="mailto:alyssatnatividad@gmail.com"
           className="text-decoration-none text-light"
-          target="_blank"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -74,7 +73,7 @@ export default function Contact() {
           viewBox="0 0 16 16"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
           ></path>
         </svg>
