a1a96b103f1dc4ac25157bc218886e1647156a45
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-13 21:33:16 +1000

    Removed app.js and called portfoliocontainer directly

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
+import ReactDOM from "react-dom/client";
+import PortfolioContainer from "./components/PortfolioContainer";
 import "animate.css";
 import "./css/App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <PortfolioContainer />
   </React.StrictMode>
 );
